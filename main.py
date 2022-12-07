230bfafcaa9c74a04c3adb1736777cfaad7a3e49
Goooofs <96235180+Goooofs@users.noreply.github.com>
2022-12-07 05:16:53 +0000

    добавил счётчик символов

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
+def symbols_counter(input):
+    dict_sym = {}
+    for i in range(len(input)):
+        dict_sym[chr(input[i])] = dict_sym.get(chr(input[i]), 0) + 1
+    return dict(sorted(dict_sym.items(), key=lambda item: item[1]))
+	
 def openbytes(myfile):
 	f = open(myfile, 'rb')
 	return f.read()
 
 def compress(myfile):
-	hex = openbytes(myfile)
-	print(f"{hex} compressed")
+	dict_sym = symbols_counter(hex)
+	print(dict_sym)
 
 def decompress(myfile):
 	hex = openbytes(myfile)
